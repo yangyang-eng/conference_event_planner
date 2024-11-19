637e9844be656bc749e34f456739e55eb57343d3
yangyang-eng <anh.duong.lrn99@gmail.com>
2024-11-20 03:09:51 +0700

    Update store.js
    Adding comment when reviewing the code

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 // store.js
+//The above code create the global Redux store so all components in the application can access the state managed by the venueReducer()
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 
+//Create the Redux Store with the configureStore function
+//Include venue() reducer
 export default configureStore({
   reducer: {
     venue: venueReducer,
   },
 });
+
+
